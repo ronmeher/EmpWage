eec63668b9e24b7f34c45d4d59bc1ca5d0d3e33c
ronmeher <ronmeher@gmail.com>
2022-04-29 05:13:16 +0530

    This is the first commit for the master. Here, we displayed a Welcome message.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation!");
+        }
+    }
+}
