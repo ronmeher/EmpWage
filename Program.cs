d65b13ca3e324589190976823a24d58a813eec74
ronmeher <ronmeher@gmail.com>
2022-04-29 05:38:30 +0530

    Added a feature to checl whether an employee is present or absent

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace EmpWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation!");
+            int IsPresent = 1;
+
+            Random random = new Random();
+
+            int EmpCheck = random.Next(2);
+
+            if (EmpCheck == IsPresent)
+            {
+                Console.WriteLine("Employee is Present!");
+            }
+            else
+            {
+                Console.WriteLine("Empoyee is Absent!");
+            }
         }
     }
 }
