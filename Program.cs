7089b237f193fd9295fb6045a5b2db0ff884ec77
ronmeher <ronmeher@gmail.com>
2022-04-30 04:59:43 +0530

    Full-Time and Part-Time added

--- Program.cs
+++ Program.cs
@@ -6,27 +6,34 @@ namespace EmpWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program!");     
-            int IsPresent = 1;
+            Console.WriteLine("Welcome to Employee Wage Computation Program!");
+            
+            int IsFullTime = 1;
+            int IsPartTime = 2;
             int WorkingHr = 0;
             int WagePerHr = 100;
             int Wage = 0;
 
             Random random = new Random();
 
-            int EmpCheck = random.Next(2);
-         
+            int EmpCheck = random.Next(3);
 
-            if (EmpCheck == IsPresent)
+
+            if (EmpCheck == IsFullTime)
             {
-                Console.WriteLine("Employee is Present!");
+                Console.WriteLine("Employee worked for Full-Time");
                 WorkingHr = 8;
 
-                
+
+            }
+            else if (EmpCheck == IsPartTime)
+            {
+                Console.WriteLine("Employee worked for Part-Time");
+                WorkingHr = 4;
             }
             else
             {
-                Console.WriteLine("Empoyee is Absent!");
+                Console.WriteLine("Empoyee was Absent!");
                 WorkingHr = 0;
             }
 
