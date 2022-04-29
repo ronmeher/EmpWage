9f3d8a7b59071c9fa99f0f5cd18f46e1a3a8f5da
ronmeher <ronmeher@gmail.com>
2022-04-29 06:33:44 +0530

    Added a feeature to calculate wge of an employee

--- Program.cs
+++ Program.cs
@@ -7,19 +7,30 @@ namespace EmpWage
         static void Main(string[] args)
         {
             int IsPresent = 1;
+            int WorkingHr = 0;
+            int WagePerHr = 100;
+            int Wage = 0;
 
             Random random = new Random();
 
             int EmpCheck = random.Next(2);
+         
 
             if (EmpCheck == IsPresent)
             {
                 Console.WriteLine("Employee is Present!");
+                WorkingHr = 8;
+
+                
             }
             else
             {
                 Console.WriteLine("Empoyee is Absent!");
+                WorkingHr = 0;
             }
+
+            Wage = WorkingHr * WagePerHr;
+            Console.WriteLine("Employee Wage is " + Wage);
         }
     }
 }
